c160fa607cac47f745f3031742cac738fed145cd
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-07-23 23:32:13 +0300

    RedisInMemory #3

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -18,10 +18,17 @@ namespace InMemoryApp.Web.Controllers
             MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
 
             //1 dakikalık bir süre verildi
-            options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(5);
 
             //10 saniyelik bir süre verildi
-            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            //options.Priority = CacheItemPriority.Low;
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}-->{value} => sebep:{reason}");
+            });
 
             _memoryCache.Set<string>("zaman",DateTime.Now.ToString(), options);
 
@@ -31,8 +38,10 @@ namespace InMemoryApp.Web.Controllers
         {
             //hafızadan okudum
             _memoryCache.TryGetValue("zaman", out string zamancache);
+            _memoryCache.TryGetValue("callback", out string callback);
 
             ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
             return View();
         }
     }
