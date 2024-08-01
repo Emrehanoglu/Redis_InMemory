43c89818ae6b8f6d539448bd2005a749e192abe1
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-08-02 00:47:39 +0300

    RedisInMemory #10

---
+++ RedisExampleApp.Api/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Kitap", Price = 50 },
+                new Product { Id = 1, Name = "Kalem", Price = 50 },
+                new Product { Id = 1, Name = "Silgi", Price = 50 });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
