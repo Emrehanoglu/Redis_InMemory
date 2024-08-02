44c31e46dcc98e26502938e2b6a3f5b902f8d1e5
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-08-02 03:07:37 +0300

    RedisInMemory #13

---
+++ RedisExampleApp.Api/Repositories/ProductRepositoryWithCache.cs
@@ -0,0 +1,33 @@
+﻿using RedisExampleApp.Api.Models;
+using RedisExampleApp.Cache;
+
+namespace RedisExampleApp.Api.Repositories
+{
+    public class ProductRepositoryWithCache : IProductRepository
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly RedisService _redisService;
+
+        public ProductRepositoryWithCache(IProductRepository productRepository, 
+            RedisService redisService)
+        {
+            _productRepository = productRepository;
+            _redisService = redisService;
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
